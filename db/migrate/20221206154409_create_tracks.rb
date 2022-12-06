3d23ce79a20b55debc7a36976b966e3c25c346d9
BenjaminCharmes <benjamin.charmes@gmail.com>
2022-12-06 15:55:12 +0000

    Update db/migrate for tracks

--- db/migrate/20221206154409_create_tracks.rb
+++ db/migrate/20221206154409_create_tracks.rb
@@ -1,7 +1,9 @@
 class CreateTracks < ActiveRecord::Migration[7.0]
   def change
     create_table :tracks do |t|
-      t.string :model
+      t.string :country
+      t.string :city
+      t.integer :length
 
       t.timestamps
     end
