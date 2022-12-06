27bcc898a83b1b96a6fded2dbb531edb3fa413bf
BenjaminCharmes <benjamin.charmes@gmail.com>
2022-12-06 18:08:06 +0000

    Update migration user_communication_means

--- db/migrate/20221206170503_create_join_table_user_communication_means.rb
+++ db/migrate/20221206170503_create_join_table_user_communication_means.rb
@@ -2,7 +2,7 @@ class CreateJoinTableUserCommunicationMeans < ActiveRecord::Migration[7.0]
   def change
     create_table :join_table_user_communication_means do |t|
       t.belongs_to :user, index: true
-      t.belongs_to :communicationmean, index: true
+      t.belongs_to :communication_mean, index: true
 
       t.timestamps
     end
