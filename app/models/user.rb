5543c7e933c2526c2c7f9aca8de866b012cb5181
KevinTAZARO <kenee23@hotmail.fr>
2022-12-06 17:17:48 +0100

    modification validates

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   #validates
-  validates :firstname, presence: true, length: {minimum: 3}
-  validates :lastname, presence: true, length: {minimum: 3}
-  validates :birthdate, presence: true
+
   validates :email, presence: true, uniqueness: true, confirmation: true, format: { with: A([^@\s]+)@((?:[-a-z0-9]+.)+[a-z]{2,})\z, message: "only allows email format"}
   validates :password, presence: true, length: {minimum: 6}
 end
