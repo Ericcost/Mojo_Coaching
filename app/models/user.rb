f78ea1c5238c0f7ae806026eef3713f6888e49c4
KevinTAZARO <kenee23@hotmail.fr>
2022-12-06 18:10:46 +0100

    modification validate

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,6 @@ class User < ApplicationRecord
 
   #validates
 
-  validates :email, presence: true, uniqueness: true, confirmation: true, format: { with: A([^@\s]+)@((?:[-a-z0-9]+.)+[a-z]{2,})\z, message: "only allows email format"}
+  validates :email, presence: true, uniqueness: true, confirmation: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+.)+[a-z]{2,})\z/i, message: "only allows email format"}
   validates :password, presence: true, length: {minimum: 6}
 end
