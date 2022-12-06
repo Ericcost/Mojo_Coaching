1abe64bf2cd3d30da79c730b7a7487958914a08b
BenjaminCharmes <benjamin.charmes@gmail.com>
2022-12-06 16:10:41 +0000

    Update db/migrate availabilities

--- db/migrate/20221206155833_create_availabilities.rb
+++ db/migrate/20221206155833_create_availabilities.rb
@@ -1,6 +1,9 @@
 class CreateAvailabilities < ActiveRecord::Migration[7.0]
   def change
     create_table :availabilities do |t|
+      t.belongs_to :user, index: true
+      t.datetime :start_date
+      t.integer :duration, default: 30
 
       t.timestamps
     end
