5822f1135dd254820fe696fb2a930cf01d3ca50a
BenjaminCharmes <benjamin.charmes@gmail.com>
2022-12-06 17:03:09 +0000

    Update db/migrate for join_table_user_tracks

--- db/migrate/20221206165105_create_join_table_user_tracks.rb
+++ db/migrate/20221206165105_create_join_table_user_tracks.rb
@@ -1,6 +1,8 @@
 class CreateJoinTableUserTracks < ActiveRecord::Migration[7.0]
   def change
     create_table :join_table_user_tracks do |t|
+      t.belongs_to :user, index: true
+      t.belongs_to :track, index: true
 
       t.timestamps
     end
