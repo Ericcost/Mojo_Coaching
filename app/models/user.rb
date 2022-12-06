e2a87407355a2112e06ff053bd4faf78dd91a11d
Ericcost <eric.costerousse@laposte.net>
2022-12-06 17:01:28 +0100

    setting validates on model user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  #validates
+  validates :firstname, presence: true, length: {minimum: 3}
+  validates :lastname, presence: true, length: {minimum: 3}
+  validates :birthdate, presence: true
+  validates :email, presence: true, uniqueness: true, confirmation: true, format: { with: A([^@\s]+)@((?:[-a-z0-9]+.)+[a-z]{2,})\z, message: "only allows email format"}
+  validates :password, presence: true, length: {minimum: 6}
 end
