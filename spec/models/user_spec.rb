2057f163fcc66160cf3bf7793d479162ebb4d9ff
Ericcost <eric.costerousse@laposte.net>
2022-12-08 00:44:07 +0100

    try to run some tests but 5 failure still

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -35,35 +35,35 @@ require 'rails_helper'
 
           describe "availabilities" do
             it "should have_many availabilities" do
-              availability = Availability.create(user_id: @user.id)
+              availability = Availability.create(user: @user)
               expect(@user.availabilities.include?(availability)).to eq(true)
             end
           end
 
           describe "JoinTableUserTracks" do
             it "should have_many JoinTableUserTracks" do
-              join_table_user_track = JoinTableUserTrack.create(user_id: @user.id)
+              join_table_user_track = JoinTableUserTrack.create(user: @user)
               expect(@user.join_table_user_tracks.include?(join_table_user_track)).to eq(true)
             end
           end
 
           describe "JoinTableUserCars" do
             it "should have_many JoinTableUserCars" do
-              join_table_user_car = JoinTableUserCar.create(user_id: @user)
+              join_table_user_car = JoinTableUserCar.create(user: @user)
               expect(@user.join_table_user_cars.include?(join_table_user_car)).to eq(true)
             end
           end
 
           describe "JoinTableUserComMeans" do
             it "should have_many JoinTableUserComMeans" do
-              join_table_user_com_mean = JoinTableUserComMean.create(user_id: @user)
+              join_table_user_com_mean = JoinTableUserComMean.create(user: @user)
               expect(@user.join_table_user_com_means.include?(join_table_user_com_mean)).to eq(true)
             end
           end
 
           describe "Meetings" do
             it "should have_many Meetings" do
-              meeting = Meeting.create(user_id: @user)
+              meeting = Meeting.create(user: @user)
               expect(@user.meetings.include?(meeting)).to eq(true)
             end
           end
