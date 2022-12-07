bf320c3f5750d770f74a3e82788a50f02e7e2f3a
Ericcost <eric.costerousse@laposte.net>
2022-12-07 18:58:47 +0100

    add validates to availability model

--- app/models/availability.rb
+++ app/models/availability.rb
@@ -2,4 +2,8 @@ class Availability < ApplicationRecord
 
   belongs_to :user
 
+  validates :user_id, presence: true, numericality: { only_integer: true }
+  validates :start_date, presence: true
+  validates :duration, presence: true, numericality: { only_integer: true }
+
 end
