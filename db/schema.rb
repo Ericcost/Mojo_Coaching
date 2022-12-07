170d17384c2dd7d189257e5addcf99462762f637
BenjaminCharmes <benjamin.charmes@gmail.com>
2022-12-07 16:59:47 +0000

    Update db

--- db/schema.rb
+++ db/schema.rb
@@ -37,7 +37,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_06_183817) do
 
   create_table "feedbacks", force: :cascade do |t|
     t.bigint "meeting_id"
-    t.text "feedback"
+    t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["meeting_id"], name: "index_feedbacks_on_meeting_id"
