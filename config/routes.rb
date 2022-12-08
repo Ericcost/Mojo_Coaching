38a9877e62de2dfa9e6d675d97afc418e329120e
Jiyan01 <jiyandag@gmail.com>
2022-12-08 10:22:44 +0100

    route change

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   resources :tracks
   devise_for :users
   resources :users
-  root 'users#index'
+  root 'availabilities#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
