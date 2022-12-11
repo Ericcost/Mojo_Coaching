8bfb1f29f86f3a7331e81c998eef0b5c907ff423
BenjaminCharmes <benjamin.charmes@gmail.com>
2022-12-11 12:32:06 +0000

    Add a method to create slot based on user availability (Does't take account of nighttime or weekend)

--- app/helpers/availabilities_helper.rb
+++ app/helpers/availabilities_helper.rb
@@ -1,2 +1,14 @@
 module AvailabilitiesHelper
+
+  def create_slot
+    array_of_slot = []
+    slot_duration = 0
+    (params[:start_date].to_datetime.to_i .. ((params[:end_date].to_datetime) - 30.minute).to_i).step(30.minute) do
+      slot = params[:start_date].to_datetime + slot_duration
+      array_of_slot << slot
+      slot_duration += 30.minute
+    end
+    return array_of_slot
+  end
+
 end
