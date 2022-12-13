6b610b21a67f04cf05f85f8534ab80c82e537038
Ericcost <eric.costerousse@laposte.net>
2022-12-13 07:01:22 +0100

    test JS on new#meeting

--- app/javascript/main.js
+++ app/javascript/main.js
@@ -2,9 +2,11 @@ let url = document.getElementsByClassName('url');
 let rdv = document.getElementsByClassName('rdv');
 console.log(rdv[0][2]);
 
-if (rdv[0][0].innerHTML === "coaching") {
-      url[0].classList.remove("hidden");
-};
+let option = rdv[0]
+let optionCoaching = rdv[0][2]
 
+option.addEventListener('click', function() {
+    url[0].classList.remove("hidden");
+})
 
 
