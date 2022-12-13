3c18ea75864c705755ae1613916b44f0f7883e99
KyleAktr <aktr.contact@gmail.com>
2022-12-13 11:17:50 +0100

    add views mailer new meeting

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,9 +3,12 @@ class UserMailer < ApplicationMailer
 
     def welcome_email(user)
       @user = user 
-
       @url  = 'https://mojo-coaching.fly.dev/users/sign_in' 
-
       mail(to: @user.email, subject: 'Bienvenue chez nous !') 
     end
+
+    def new_meeting_email(user)
+      @user = user
+      @url  = 'https://mojo-coaching.fly.dev/users/sign_in'
+      mail(to: @user.email, subject: "Confirmation d'une nouvelle demande de meeting")
   end
