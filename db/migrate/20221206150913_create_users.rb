86f92655e943fc2aff523b23dfe13e5784d69c0a
BenjaminCharmes <benjamin.charmes@gmail.com>
2022-12-13 14:32:29 +0000

    Remove attribute birthdate and Add attribute description in user_migration

--- db/migrate/20221206150913_create_users.rb
+++ db/migrate/20221206150913_create_users.rb
@@ -3,7 +3,7 @@ class CreateUsers < ActiveRecord::Migration[7.0]
     create_table :users do |t|
       t.string :first_name
       t.string :last_name
-      t.date :birthdate
+      t.text :description
       t.text :awards
       t.integer :price_per_hour
 
