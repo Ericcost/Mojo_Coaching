159583942e200aba59ea5007cf905d3d0ca7a277
BenjaminCharmes <benjamin.charmes@gmail.com>
2022-12-16 11:07:54 +0000

    Add method welcome_send with after_create in models_user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  after_create :welcome_send
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
@@ -21,5 +22,9 @@ class User < ApplicationRecord
   has_many :tracks, through: :join_table_user_tracks
 
   has_many :availabilities
+
+  def welcome_send
+    UserMailer.welcome_email(self).deliver_now
+  end
 
 end
