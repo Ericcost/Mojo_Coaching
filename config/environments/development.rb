b03b0882dd655f4e47a3fe9c1f409fcf4b9dca1e
Ericcost <eric.costerousse@laposte.net>
2022-12-16 16:15:10 +0100

    modification

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -42,7 +42,7 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
 
   # Mailer localhost only for now, change when on production
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = {host: 'mojo-coaching.fly.dev'}
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
